eee3a2e6ef8706bd9025d1f2cbcc40546342fde0
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2023-09-26 13:16:00 +0300

    commit 26/09/2023, fixed apiTests

--- src/test/java/tests/api/GetApiTests.java
+++ src/test/java/tests/api/GetApiTests.java
@@ -2,21 +2,17 @@ package tests.api;
 
 import baseEntities.BaseApiTest;
 import io.restassured.http.ContentType;
-import io.restassured.http.Method;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 import org.apache.http.protocol.HTTP;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.configuration.ReadProperties;
 
 import static io.restassured.RestAssured.given;
 
-public class GetApiTest extends BaseApiTest {
-    static Logger logger = LogManager.getLogger(GetApiTest.class);
+public class GetApiTests extends BaseApiTest {
+    static Logger logger = LogManager.getLogger(GetApiTests.class);
     @Test
     public void getAllUsers() {
         String endpoint = "/index.php?/api/v2/get_users";
--- src/test/java/tests/api/PostApiTest.java
+++ src/test/java/tests/api/PostApiTest.java
@@ -1,41 +1,15 @@
 package tests.api;
 
 import baseEntities.BaseApiTest;
-import models.Project;
-import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.given;
 
 public class PostApiTest extends BaseApiTest {
     static Logger logger = LogManager.getLogger(PostApiTest.class);
     @Test
-    public void addProjectTest() {
-        String endpoint = "/index.php?/api/v2/add_project";
+    public void addMilestoneTest() {
+        String endpoint = "/index.php?/api/v2/add_milestone";
 
-        Project expectedProject = new Project();
-        expectedProject.setName("WP_Project_01");
-        expectedProject.setAnnouncement("This is a description!!!");
-        expectedProject.setFlag(true);
-        expectedProject.setProjectType(1);
-
-                 given()
-                .body(String.format("{\n" +
-                                "  \"name\": \"%s\",\n" +
-                                "  \"announcement\": \"%s\",\n" +
-                                "  \"show_announcement\": %b,\n" +
-                                "  \"suite_mode\" : %d\n" +
-                                "}",
-                        expectedProject.getName(),
-                        expectedProject.getAnnouncement(),
-                        expectedProject.getFlag(),
-                        expectedProject.getProjectType()
-                ))
-                .when()
-                .post(endpoint)
-                .then()
-                .log().body()
-                .statusCode(HttpStatus.SC_OK);
     }
 }
